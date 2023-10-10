f3349bc5eb26781c48ddb5cb330eee80471e6e1b
Gregory Dutton <greg.dutton@gmail.com>
2023-10-10 15:05:38 -0500

    complete lab

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,8 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        return "".join(items)
+    items[len(items)-1] = "and " + items[len(items)-1]
+    if len(items) > 2:
+        return ", ".join(items)
+    else:
+        return " ".join(items)
